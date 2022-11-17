a151b1693803b18f1421ddfad1d3df594700e912
Suriyaprakash-Github <yoursgump@gmail.com>
2022-11-18 00:15:01 +0530

    DOM Assignment

---
+++ DOM/Intro/dom.js
@@ -0,0 +1,25 @@
+// console.dir(document)
+// console.log(document.domain)
+// console.log(document.URL)
+// console.log(document.title)
+// document.title="sdfsdg"
+// console.log(document.body)
+// console.log(document.head)
+// console.log(document.all)
+// console.log(document.forms)
+// console.log(document.links)
+
+//var headerTitle=document.getElementById('header-title')
+//console.log(headerTitle)
+//headerTitle.innerText='GOOOOOD'
+// //console.log(headerTitle.innerText)
+// headerTitle.textContent='BAAAAAAAD'
+// console.log(headerTitle.innerHTML)
+
+//headerTitle.style.borderBottom="black 3px solid"
+//headerTitle.style.border="black 3px solid"
+
+let title=document.getElementById("title")
+console.log(title)
+title.innerHTML="<b>ADD ITEM</b>"
+title.style.color="green"
