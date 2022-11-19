cb7c4b2660fe27f1c71885d7cffdc744939603c2
Suriyaprakash-Github <yoursgump@gmail.com>
2022-11-19 22:00:55 +0530

    Scaling the app

--- DOM/Appointment App/script.js
+++ DOM/Appointment App/script.js
@@ -1,27 +1,34 @@
-let myForm=document.querySelector("#my-form");
-let nameInput=document.querySelector("#name")
-let emailInput=document.querySelector("#email")
+let myForm = document.querySelector('#my-form');
+let nameInput = document.querySelector('#name');
+let emailInput = document.querySelector('#email');
 
+myForm.addEventListener('submit', submitListener);
 
-myForm.addEventListener("submit", submitListener)
+function submitListener(e) {
+  e.preventDefault();
+  let mailId = emailInput.value;
+  let obj = {
+    objName: nameInput.value,
+    objEmail: emailInput.value,
+  };
+  let objSerialized = JSON.stringify(obj);
+  // console.log(objSerialized);
+  localStorage.setItem(obj.objEmail, objSerialized);
+  let objDeserilized = JSON.parse(localStorage.getItem(obj.objEmail));
+  //console.log(objDeserilized);
+  let parsedObj = JSON.parse(localStorage.getItem(obj.objEmail));
+  let parsedName = parsedObj.objName;
+  let parsedEmail = parsedObj.objEmail;
+  console.log(parsedName);
+  console.log(parsedEmail);
 
-function submitListener(e){
-    e.preventDefault();
-  //  console.log(nameInput.value);
-   // console.log(emailInput.value);
-   // Previous Assignment:
-          //  localStorage.setItem("name",nameInput.value)
-          //   localStorage.setItem("email",emailInput.value)
-          //   console.log(localStorage.getItem("name"))
-          //   console.log(localStorage.getItem("email"))
-  let obj={
-    objName:nameInput.value,
-    objEmail:emailInput.value
-  }
-  let objSerialized=JSON.stringify(obj)
-  console.log(objSerialized);
-  localStorage.setItem("details", objSerialized);
-  let objDeserilized=JSON.parse(localStorage.getItem("details"))
-  console.log(objDeserilized)
+  //printing the input:scaling assignment
+  let div = document.createElement('li');
+  div.className = 'item';
+  let divText = document.createTextNode(parsedName + ' ' + parsedEmail);
+  div.appendChild(divText);
+
+  let container = document.querySelector('.container .items');
+  let objText = document.querySelector('.item');
+  container.insertBefore(div, objText);
 }
-
